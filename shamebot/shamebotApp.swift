c357bf0854441541169d0c1d0473fef5eef33e14
Michael <mheadv1@gmail.com>
2025-04-01 17:22:44 -0700

    skd

---
+++ shamebot/shamebotApp.swift
@@ -0,0 +1,17 @@
+//
+//  shamebotApp.swift
+//  shamebot
+//
+//  Created by Michael Head on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct shamebotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shamebotTests/shamebotTests.swift
@@ -0,0 +1,17 @@
+//
+//  shamebotTests.swift
+//  shamebotTests
+//
+//  Created by Michael Head on 4/1/25.
+//
+
+import Testing
+@testable import shamebot
+
+struct shamebotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
